55e23ce704c55ee5f698ab0b45415598ffe660ba
Glenn Spinner <glenngineer1@gmail.com>
2016-05-21 11:23:29 -0500

    got it

---
+++ reindeer.js
@@ -0,0 +1,18 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement = document.getElementById("reindeer");
+
+for (var i = 0; i < colors.length; i++) {
+    for (var j = 0; j < reindeer.length; j++) {
+      // console.log(colorsList);
+      // console.log(reindeerList);
+  }
+      var colorsList = colors[i];
+      var reindeerList = reindeer[i];
+  hohohoElementRevised = colorsList + " " + reindeerList;
+  console.log(hohohoElementRevised);
+  document.getElementById("reindeer").innerHTML += "<div>" + colorsList + " " + reindeerList + "</div>";
+}
+
+
